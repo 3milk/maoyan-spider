812be0d09bf959a6295708a9a562d89145e6e4f4
3milk <429407748@qq.com>
2019-04-24 00:19:51 +0800

    add parse process for comments

--- myFilm/spiders/film.py
+++ myFilm/spiders/film.py
@@ -1,12 +1,13 @@
 import scrapy
 from scrapy import Request
 import json
-from myFilm.items import MyFilmItem
+from ..items import MyFilmItem, MyCommentItem
 import re
 import os
 from fontTools.ttLib import TTFont
 import requests
 from lxml.html import fromstring, tostring
+import string
 
 class MyFileSpider(scrapy.Spider):
     name = 'myFilm'
@@ -16,12 +17,13 @@ class MyFileSpider(scrapy.Spider):
     custom_settings = {
         "ITEM_PIPELINES": {
             'myFilm.pipelines.MyFilmPipeline': 300,
-            #'myFilm.pipelines.MyCommentPipeline': 300
+            'myFilm.pipelines.MyCommentPipeline': 301
         },
         "DEFAULT_REQUEST_HEADERS": {
             'accept': 'application/json, text/javascript, */*; q=0.01',
             'accept-encoding': 'gzip, deflate',
             'accept-language': 'zh-CN,zh;q=0.8,en;q=0.6,zh-TW;q=0.4',
+            'cookie':'uuid=4C55CBC0601D11E9A631B37CFB7F525898057B04D2874DBF91DEB5BE48270E96',
             'referer': 'https://mm.taobao.com/search_tstar_model.htm?spm=719.1001036.1998606017.2.KDdsmP',
             'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.97 Safari/537.36',
             'x-requested-with': 'XMLHttpRequest',
@@ -45,7 +47,7 @@ class MyFileSpider(scrapy.Spider):
         for number, gly in enumerate(self.stdFont.getGlyphOrder()):
             keys.insert(number, gly)
         #keys = self.stdFont['glyf'].keys()
-        values = list(' 4386275091.')
+        values = list(' .9243108567')#(' 4386275091.')
         # 构建基准 {name: num}
         self.stdDict = dict((k,v) for k,v in zip(keys, values))
 
@@ -78,7 +80,6 @@ class MyFileSpider(scrapy.Spider):
                 if self.stdFont['glyf'][k] == self.curFont['glyf'][key]:
                     self.curDict[key] = v.strip()
                     break
-        # print(dict2)
 
     # 把获取到的数据用字体对应起来，得到真实数据
     def modify_data(self, data):
@@ -98,6 +99,44 @@ class MyFileSpider(scrapy.Spider):
         # 返回替换后的字符串
         return data
 
+    # 解析电影热门评论列表
+    def parseComment(self, response):
+        item = response.meta['item'] 
+
+        comments = response.xpath('//div[@class="comment-list-container"]/ul/li')
+        for number, comment in enumerate(comments):
+            commItem = MyCommentItem()
+            commentID = comment.xpath('./@data-val').extract()
+            commItem['cid'] = int(commentID[0].split(':')[1].replace("}",""))#json.loads(commentID[number])['commentid']
+            commItem['fid'] = int(item['fid'])
+            #score
+            score = comment.xpath('//div[@class="main-header clearfix"]//ul[@class="score-star clearfix"]/@data-score').extract()
+            if score:
+                commItem['score'] = int(score[number]) 
+            else: # drop this comment
+                continue
+            #comment
+            commentContent = comment.xpath('//div[@class="comment-content"]/text()').extract()
+            if commentContent:
+                commItem['comment'] = commentContent[number]
+            else: # drop this comment
+                continue
+            #liked
+            liked = comment.xpath('//div[@class="main-header clearfix"]/div[@class="approve "]/span[@class="num"]/text()').extract()
+            if liked:
+                commItem['liked'] = int(liked[number])
+            else:
+                commItem['liked'] = 0
+            #commentTime
+            commentTime = comment.xpath('//div[@class="main-header clearfix"]/div[@class="time"]/@title').extract()
+            if commentTime:
+                commItem['commentTime'] = commentTime[number]
+            else:
+                commItem['commentTime'] = "1000-01-01"
+            yield commItem
+
+
+    # 解析电影详细信息
     def parseDetail(self, response):
         item = response.meta['item'] 
 
@@ -111,22 +150,28 @@ class MyFileSpider(scrapy.Spider):
         countryTime = response.xpath('//div[@class="movie-brief-container"]/ul/li[2]/text()').extract()[0]
         if countryTime:
             strs = countryTime.split()
-            item['country'] = strs[0]
             item['length'] = 0
-            for i in range(1, len(strs)): 
-                l = re.sub(r"\D", "", strs[i]) # '\n        中国香港,中国台湾\n          / 100分钟\n        '
-                if len(l) != 0:
-                    item['length'] = int(l)
-                    break
+            if strs:
+                item['country'] = strs[0]
+                for i in range(1, len(strs)): 
+                    l = re.sub(r"\D", "", strs[i]) # '\n        中国香港,中国台湾\n          / 100分钟\n        '
+                    if len(l) != 0:
+                        item['length'] = int(l)
+                        break
+            else:
+                item['country'] = ""
+                print(countryTime)
+
         else:
             item['country'] = ''
             item['length'] = 0
 
         # releaseTime releaseTimeOnlyYear
         releaseTime = response.xpath('//div[@class="movie-brief-container"]/ul/li[3]/text()').extract()
-        item['releaseTime'] = re.sub("[^0-9-]", "", releaseTime[0]) if releaseTime else None
-        if(item['releaseTime'] and ('-' not in releaseTime[0])):
+        item['releaseTime'] = re.sub("[^0-9-]", "", releaseTime[0]) if releaseTime else '1000-01-01'
+        if('-' not in item['releaseTime']):
             item['releaseTimeOnlyYear'] = 1
+            item['releaseTime'] = item['releaseTime'] + "-01-01"
         else:
             item['releaseTimeOnlyYear'] = 0
         
@@ -163,30 +208,86 @@ class MyFileSpider(scrapy.Spider):
                 item['boxOffice'] = int(item['boxOffice'])
         item['monetaryUnit'] = None
         
-        item['actors'] = ""#response.xpath()
+        item['actors'] = ''
+        celebrityGroup = response.xpath('//div[@class="tab-content-container"]/div[@class="tab-celebrity tab-content"]/div[@class="celebrity-container"]//div[@class="celebrity-group"]')
+        for number, group in enumerate(celebrityGroup):
+            groupType = group.xpath('//div[@class="celebrity-type"]/text()').extract()
+            if("演员" in groupType[number]):
+                actorsInfo = group.xpath('//ul[@class="celebrity-list clearfix"]//li[@class="celebrity actor"]')
+                cnt = 0
+                for actorNum, actorInfo in enumerate(actorsInfo):
+                    actualName = actorInfo.xpath('//div[@class="info"]/a/text()').extract()
+                    if actualName:
+                        actualName = actualName[actorNum].replace("\n","")
+                        actualName = actualName.strip() 
+                        item['actors'] = item['actors'] + actualName + ','
+                    else: 
+                        actualName = ''
+                    if cnt < 9:
+                        cnt = cnt+1
+                    else:
+                        break
+                break
+        item['actors'] = item['actors'].strip(string.punctuation)           
+        yield item # film item
 
-        yield item #yield
+        # 解析评论信息
+        #self.parseComment(response)
+        #comment item
+        comments = response.xpath('//div[@class="comment-list-container"]/ul/li')
+        for number, comment in enumerate(comments):
+            commItem = MyCommentItem()
+            commentID = comment.xpath('./@data-val').extract()
+            commItem['cid'] = int(commentID[0].split(':')[1].replace("}",""))#json.loads(commentID[number])['commentid']
+            commItem['fid'] = int(item['fid'])
+            #score
+            score = comment.xpath('//div[@class="main-header clearfix"]//ul[@class="score-star clearfix"]/@data-score').extract()
+            if score:
+                commItem['score'] = int(score[number]) 
+            else: # drop this comment
+                continue
+            #comment
+            commentContent = comment.xpath('//div[@class="comment-content"]/text()').extract()
+            if commentContent:
+                commItem['comment'] = commentContent[number]
+            else: # drop this comment
+                continue
+            #liked
+            liked = comment.xpath('//div[@class="main-header clearfix"]/div[@class="approve "]/span[@class="num"]/text()').extract()
+            if liked:
+                commItem['liked'] = int(liked[number])
+            else:
+                commItem['liked'] = 0
+            #commentTime
+            commentTime = comment.xpath('//div[@class="main-header clearfix"]/div[@class="time"]/@title').extract()
+            if commentTime:
+                commItem['commentTime'] = commentTime[number]
+            else:
+                commItem['commentTime'] = "1000-01-01"
+            yield commItem
+        
 
+    # 解析电影基础信息
     def parse(self, response):
-        movies = response.xpath('//div[@class="movies-list"]/dl[@class="movie-list"]')
+        movies = response.xpath('//div[@class="movies-list"]/dl[@class="movie-list"]//dd')
         for movie in movies:
             item = MyFilmItem()
-            item['name'] = movie.xpath('./dd/div[@class="channel-detail movie-item-title"]/@title').extract()[0]
-            filmUrl = movie.xpath('./dd/div[@class="channel-detail movie-item-title"]/a/@href').extract()[0]
+            item['name'] = movie.xpath('.//div[@class="channel-detail movie-item-title"]/@title').extract()[0]
+            filmUrl = movie.xpath('.//div[@class="channel-detail movie-item-title"]/a/@href').extract()[0]
             item['fid'] = filmUrl[7:] #/films/12504341
-            noScore = movie.xpath('./dd/div[@class="channel-detail channel-detail-orange"]/text()').extract()
+            noScore = movie.xpath('.//div[@class="channel-detail channel-detail-orange"]/text()').extract()
             if noScore:
                 item['score'] = 0.0
             else:
-                integer = movie.xpath('./dd/div[@class="channel-detail channel-detail-orange"]/i[@class="integer"]/text()').extract()[0]
-                fraction = movie.xpath('./dd/div[@class="channel-detail channel-detail-orange"]/i[@class="fraction"]/text()').extract()[0]
+                integer = movie.xpath('.//div[@class="channel-detail channel-detail-orange"]/i[@class="integer"]/text()').extract()[0]
+                fraction = movie.xpath('.//div[@class="channel-detail channel-detail-orange"]/i[@class="fraction"]/text()').extract()[0]
                 item['score'] = float(integer[:-1]) + 0.1*float(fraction)
 
-            posterURL = movie.xpath('./dd/div[@class="movie-item"]/a/div[@class="movie-poster"]/img/@data-src').extract()[0]
+            posterURL = movie.xpath('.//div[@class="movie-item"]/a/div[@class="movie-poster"]/img/@data-src').extract()[0]
             src = r'https://.*?.jpg' 
             pattern = re.compile(src)     #re.compile()，可以把正则表达式编译成一个正则表达式对象
             imglist = re.findall(pattern, posterURL) 
-            item['poster'] = imglist[0]
+            item['poster'] = imglist[0] if imglist else ''
 
             filmUrl = "https://maoyan.com" + filmUrl
             request = Request(filmUrl, callback=self.parseDetail)
